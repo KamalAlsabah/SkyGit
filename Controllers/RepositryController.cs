599fc0cf668b5503edf3cd03b29140e26d6b3f5b
KamalAlsabah <kamaes100@gmail.com>
2022-09-13 09:40:07 +0300

    Add project files.

---
+++ Controllers/RepositryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkyGit.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RepositryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Create(string name)
+        {
+            string path = LibGit2Sharp.Repository.Init($"wwwroot\\Repositories\\{name}", true);
+            return Ok(path);
+        }
+        [HttpGet]
+
+        public IActionResult GetRepoUrl(string name)
+        {
+            string serverAddress = string.Format("{0}://{1}/{2}{3}",
+                                    HttpContext.Request.Scheme,
+                                     HttpContext.Request.Host,
+                                     name,
+                                     ".git"
+                                     );
+
+            return Ok(serverAddress);
+        }
+        [HttpGet("/{repositoryName}.git/info/refs")]
+        public IActionResult CloneTo(string? repositoryName)
+        {
+            return Ok();
+        }
+    }
+}
